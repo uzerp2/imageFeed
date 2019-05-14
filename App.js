b594715c4e0f86962109cd9afb71547a42a2bb9e
PULS\p.semin <uzerp2apple@gmail.com>
2019-05-14 16:59:35 +0300

    // первый коммит, первый урок

---
+++ App.js
@@ -0,0 +1,92 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
+
+// import React, {Component} from 'react';
+// import {Platform, StyleSheet, Text, View} from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>Welcome to React Native!</Text>
+//         <Text style={styles.instructions}>To get started, edit App.js</Text>
+//         <Text style={styles.instructions}>{instructions}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// 1-ый урок
+
+
+import { Platform, StyleSheet, View } from 'react-native';
+// import { Constants } from 'expo';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+import React from 'react';
+
+import Feed from './src/1/screens/Feed';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Feed style={styles.feed} />
+      </View>
+    );
+  }
+}
+
+const platformVersion =
+  Platform.OS === 'ios'
+    ? parseInt(Platform.Version, 10)
+    : Platform.Version;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === 'android' || platformVersion < 11
+        ? getStatusBarHeight()
+        : 0,
+  },
+});
